e67a5c5a4072062a5ecaa766a1ba7db87b57add9
rshreemr12 <rshreemr12@gmail.com>
2022-02-08 14:23:23 +0000

    modified urls to consider views

--- demo/urls.py
+++ demo/urls.py
@@ -2,15 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django_Rest_api.store.api_views import ProductList
 
-import store.views
-import store.api_views
+import store.views as views
+import store.api_views.ProductList
 
 urlpatterns = [
-    path('api/v1/products/', store.api_views.ProductList.as_view()),
+    path('api/v1/products/', ProductList.as_view()),
 
     path('admin/', admin.site.urls),
-    path('products/<int:id>/', store.views.show, name='show-product'),
-    path('cart/', store.views.cart, name='shopping-cart'),
-    path('', store.views.index, name='list-products'),
+    path('products/<int:id>/', views.show, name='show-product'),
+    path('cart/', views.cart, name='shopping-cart'),
+    path('', views.index, name='list-products'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

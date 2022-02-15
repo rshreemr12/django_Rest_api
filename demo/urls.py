3c4953e9a3420d953de8c4ef1fb4c56a98506125
rshreemr12 <rshreemr1@gmail.com>
2022-02-15 20:12:07 +0000

    adding - serializers, - views, -stats, -productlist, -api-views,

--- demo/urls.py
+++ demo/urls.py
@@ -2,14 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django_Rest_api.store.api_views import ProductList
 
 import store.views as views
-import store.api_views.ProductList
+import store.api_views as api_views
 
 urlpatterns = [
-    path('api/v1/products/', ProductList.as_view()),
-
+    path('api/v1/products/', api_views.ProductList.as_view()),
+    path('api/v1/products/new', api_views.ProductCreate.as_view()),
+    path('api/v1/products/<int:id>/', api_views.ProductRetrieveUpdateDestroy.as_view()),
+    path('api/v1/products/<int:id>/stats', api_views.ProductStats.as_view()),
+    
     path('admin/', admin.site.urls),
     path('products/<int:id>/', views.show, name='show-product'),
     path('cart/', views.cart, name='shopping-cart'),
